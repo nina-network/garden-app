38d3c4b5e070513aeb2138f2797f63aac9a8c260
nina-network <serenalee357@gmail.com>
2025-07-24 17:18:41 -0400

    tab change

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Gauge Your Garden</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Gauge Your Garden" />
         <p className="description">
-          Welcome to the app for plant lovers! I created this app to help my reltives who're interested in gardening.
+          Welcome to the app for plant lovers! I created this app to help my relatives who're interested in gardening.
           Here, you will be able to journal the data of your plant growth based on height which will be predicted for your future use.
           I am hoping to add two sections: One for plant information and the other for interacting with other gardeners.
         </p>
