bf8318e3fc4258e3d6913430f9b0eaa2eab47a75
nina-network <serenalee357@gmail.com>
2025-07-24 20:02:56 -0400

    words touching the screen no more >:)

--- pages/index.js
+++ pages/index.js
@@ -13,20 +13,18 @@ export default function Home() {
       <main>
         <Header title="Gauge Your Garden" />
         <div className="description">
-          Welcome to the app for plant lovers! 
-          <br/>
-          I created this app to help my relatives who're interested in gardening.
-          <br/><br/>
-          Here, you will be able to journal the data of your plant growth based on height which will be predicted for your future use.
-          <br/><br/>
-          This app will be mobile-friendly and include:
+          <p>Welcome to the app for plant lovers!</p>
+          <p>I created this app to help my relatives who're interested in gardening.</p>
+          <p>Here, you will be able to journal the data of your plant growth based on height which will be predicted for your future use.</p>
+          <p>This app will be mobile-friendly and include:</p>
 
           <ul>
-          <li>Plant Information and Identification</li>
-          <li>Journaling and Prediction</li>
-          <li>Interaction with Other Users</li>
+            <li>Plant Information and Identification</li>
+            <li>Journaling and Prediction</li>
+            <li>Interaction with Other Users</li>
           </ul>
         </div>
+
       </main>
 
       <Footer />
