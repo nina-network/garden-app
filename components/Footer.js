6d1ff2ba8e740370768d30b08a9640aaa087ceed
nina-network <serenalee357@gmail.com>
2025-07-24 20:12:54 -0400

    oops i forgot abt my footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/favicon_gyg.ico" alt="GYG Logo" className={styles.logo} />
+        <img src="/favicon_gyg.png" alt="GYG Logo" className={styles.logo} />
         <p> to make this mobile friendly, this is where the menu will be </p>
       </footer>
     </>
